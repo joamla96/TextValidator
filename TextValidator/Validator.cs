c934c9b168b6837008edc26c1569c3d59cdbf0bb
jona8690 <jona8690@edu.eal.dk>
2017-01-16 08:39:56 +0100

    Text Validator

---
+++ TextValidator/Validator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TextValidator
+{
+	public class Validator {
+		public bool Text(string v) {
+			return Regex.IsMatch(v, @"^[a-zA-Z]+$");
+		}
+
+		public bool Number(string input) {
+			return Regex.IsMatch(input, @"^[0-9]+$");
+		}
+	}
+}
---
+++ TextValidator/ValidatorTests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TextValidator {
+	
+	[TestClass]
+	public class ValidatorTests {
+		Validator Valid = new Validator();
+
+		[TestMethod]
+		public void TextOnly() {
+			Assert.IsTrue(Valid.Text("Hello"));
+			Assert.IsTrue(Valid.Text("TestIng"));
+
+			Assert.IsFalse(Valid.Text("Hello My Freind"));
+			Assert.IsFalse(Valid.Text("H3llo"));
+			Assert.IsFalse(Valid.Text("H3llo My Fre1nd"));
+		}
+
+		[TestMethod]
+		public void NumbersOnly() {
+			throw new NotImplementedException();
+		}
+	}
+}
